4fc9a2b3ab6dab10e9484a92577e4fef8255f46f
Ben Quigley <ben.quigley.all@gmail.com>
2021-05-01 12:49:06 -0400

    Implement react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import quigley from "./static/images/quigley.jpg";
 import quigleyExpoCropped from "./static/images/quigleyExpoCropped.jpg";
+import { Link, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import { Helmet } from "react-helmet";
 
-const Contact = () => {
-  <div id="contact" className="card display">
-    <h2>Contact</h2>
-    <div className="name-badge">
-      <div className="details">
-        <p className="emph">Ben Quigley</p>
-        <p>Solutions engineer</p>
-        <ul>
-          <li>Home: Brooklyn, NY</li>
-          <li>
-            Email:{" "}
-            <a href="mailto:benjamin.s.quigley@gmail.com">
-              benjamin.s.quigley@gmail.com
-            </a>
-          </li>
-        </ul>
+const Home = () => (
+  <div className="container">
+    <div className="card display">
+      <div>
+        <h1>Ben Quigley</h1>
+        <p>
+          Solutions engineer developing and managing user-friendly, effective,
+          enterprise software systems.
+        </p>
       </div>
-      <img src={quigley} className="small" alt="Ben" />
+      <img
+        src={quigleyExpoCropped}
+        alt="Ben at a HR technology expo with the Sage People team"
+      />
     </div>
-  </div>;
-};
+  </div>
+)
 
-const About = () => {
+const About = () => (
   <div id="about" className="card display">
     <h2>About Ben</h2>
     <h4>
@@ -70,56 +67,81 @@ const About = () => {
         <td>Open to new projects</td>
       </tr>
     </table>
-  </div>;
-};
+  </div>
+)
+
+const Contact = () => (
+  <div id="contact" className="card display">
+    <h2>Contact</h2>
+    <div className="name-badge">
+      <div className="details">
+        <p className="emph">Ben Quigley</p>
+        <p>Solutions engineer</p>
+        <ul>
+          <li>Home: Brooklyn, NY</li>
+          <li>
+            Email:{" "}
+            <a href="mailto:benjamin.s.quigley@gmail.com">
+              benjamin.s.quigley@gmail.com
+            </a>
+          </li>
+        </ul>
+      </div>
+      <img src={quigley} className="small" alt="Ben" />
+    </div>
+  </div>
+)
+
+const AboutSite = () => (
+  <div className="card mini">
+    <p>
+      This site was built using <a href="https://reactjs.org/">React</a>.
+    </p>
+    <p>
+      Its source code is hosted{" "}
+      <a href="https://github.com/BenQuigley/benquigley.github.io">here</a>.
+    </p>
+  </div>
+)
 
 export default function App() {
   return (
-    <div className="App">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Ben Quigley Home</title>
-        <link rel="canonical" href="http://signmaker.dev" />
-      </Helmet>
-      <div className="body">
-        <div className="card mini">
-          <p>
-            <a href="index.html">home</a> | <a href="index.html#about">about</a>{" "}
-            | <a href="index.html#contact">contact</a> |{" "}
-            <a href="https://benideas.home.blog">blog</a> |{" "}
-            <a href="https://github.com/BenQuigley">GitHub</a> |{" "}
-            <a href="static/pages/QuigleyResume.html">resume</a>
-          </p>
-        </div>
-        <div className="notifications"></div>
-        <div className="container">
-          <div className="card display">
-            <div>
-              <h1>Ben Quigley</h1>
+    <BrowserRouter>
+      <div className="App">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Ben Quigley Home</title>
+          <link rel="canonical" href="http://signmaker.dev" />
+        </Helmet>
+          <div className="body">
+            <div className="card mini">
               <p>
-                Solutions engineer developing and managing user-friendly,
-                effective, enterprise software systems.
+                <Link to="/">home</Link> | <Link to="/about">about</Link> |{" "}
+                <Link to="/contact">contact</Link> |{" "}
+                <a href="https://benideas.home.blog">blog</a> |{" "}
+                <a href="https://github.com/BenQuigley">GitHub</a> |{" "}
+                <Link to="/resume">resume</Link>
               </p>
             </div>
-            <img
-              src={quigleyExpoCropped}
-              alt="Ben at a HR technology expo with the Sage People team"
-            />
           </div>
-        </div>
-        <div className="card mini">
-          <p>
-            This site was built using <a href="https://reactjs.org/">React</a>.
-          </p>
-          <p>
-            Its source code is hosted{" "}
-            <a href="https://github.com/BenQuigley/benquigley.github.io">
-              here
-            </a>
-            .
-          </p>
-        </div>
+
+          {/* Route components are rendered if the path prop matches the current URL */}
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <AboutSite />
+          </Route>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
